49b10959e885ed34a6899ac0f3da64d9977894ee
Vitor Amaral de Melo <vitor.amaral10@gmail.com>
2024-03-19 16:14:46 -0300

    refactor: disabled cache

--- lib/presentation/presenters/getx_settings_presenter.dart
+++ lib/presentation/presenters/getx_settings_presenter.dart
@@ -1,23 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import '../../domain/usecases/usecases.dart';
 import '../ui/pages/pages.dart';
 
 class GetxSettingsPresenter extends GetxController
     implements SettingsPresenter {
-  LoadThemeMode loadThemeMode;
-  SaveThemeMode saveThemeMode;
+  // LoadThemeMode loadThemeMode;
+  // SaveThemeMode saveThemeMode;
 
   final _darkMode = false.obs;
 
   @override
   bool get darkMode => _darkMode.value;
 
-  GetxSettingsPresenter({
-    required this.loadThemeMode,
-    required this.saveThemeMode,
-  });
+  GetxSettingsPresenter(
+      //   {
+      //   required this.loadThemeMode,
+      //   required this.saveThemeMode,
+      // }
+      );
 
   @override
   void onInit() {
@@ -27,16 +28,17 @@ class GetxSettingsPresenter extends GetxController
   }
 
   @override
-  void loadCurrentThemeMode() async {
-    final themeMode = await loadThemeMode.load();
-    _darkMode.value = themeMode == ThemeMode.dark;
+  void loadCurrentThemeMode() {
+    // final themeMode = await loadThemeMode.load();
+    // _darkMode.value = themeMode == ThemeMode.dark;
+    _darkMode.value = true;
   }
 
   @override
   void toggleDarkMode(bool value) {
     try {
       ThemeMode themeMode = value ? ThemeMode.dark : ThemeMode.light;
-      saveThemeMode.save(themeMode);
+      // saveThemeMode.save(themeMode);
       Get.changeThemeMode(themeMode);
       _darkMode.value = value;
     } on Exception {
